c2409c1cb3273749a9628f83578907c198e0d0b1
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-04-30 20:15:16 +0000

    add interest.py

---
+++ interest.py
@@ -0,0 +1,53 @@
+### Check the README for additional information ###
+
+# 1. Write a function that will help you compute how much money you would have after 1 year if you invest a given amount in an account with a particular annual return.
+
+
+
+# 2. Update the invest() function to leverage the time parameter in order to calculate the value of an investment growing at a particular annual rate for 10 or more years.
+
+
+
+# 3. Use your invest() function to compute the value of:
+    # i. A $5,000 investment at 3% for 10 years.
+    # ii. A $10,000 investment at 3% for 5 years. (double the amount, half the time)
+    # iii. A $2,500 investment at 3% for 20 years. (half the amount, double the time)
+
+
+
+# 4. Use your invest() function to compute the value of:
+    # i. A $4,000 investment at 5% for 30 years.
+    # ii. A $4,000 investment at 7.5% for 30 years.
+    # iii. A $4,000 investment at 10% for 30 years.
+
+
+
+# 5. Who has more money (Clara, Jerome, Z) in their account at age 45? At age 50? At age 65?
+
+
+
+# 6. Create three new functions: stock(), indexFund(), and bond(). Each function should take two inputs, initial investment and time of the investment.
+
+
+
+# 7. Create a new method called monthly() that takes three parameters: an initial investment, an annual interest rate, and a length of time in years.
+
+
+
+# 8. Compare the three payment strategies (Sage, Steve, Sid) to determine how much each graduate will end up paying to the loan company.
+
+
+
+# 9. Assume your credit card has an annual interest rate of 12%. Calculate the actual cost of a $1,200 mattress if you pay:
+    # i. $50 per month
+    # ii. $100 per month
+    # iii. $200 per month
+
+
+
+# 10. Consider the following two debts:
+    # a. You owe a credit card company $2,000 and they charge a 22% annual interest rate.
+    # b. You owe a student loan company $20,000 and they charge a 6% annual interest rate.
+# If you have $400 per month to pay down debts, which debt should you pay down first? How much would you save by paying that debt first instead of the other debt?
+
+
